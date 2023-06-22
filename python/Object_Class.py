ff1fa63da4be32d325ad61ebcdbf18bc445c3163
zupm-dot <40860386+zupm-dot@users.noreply.github.com>
2023-06-22 09:23:42 -0400

    Update Object_Class.py

--- python/Object_Class.py
+++ python/Object_Class.py
@@ -6,32 +6,34 @@ class BigIP_Object:
 
 class Node(BigIP_Object):
     def __init__(self, name, partition, address, monitor=None):
+        self.name = name
+        self.partition = partition
         self.address = address
         self.monitor = monitor
 
 
 class Monitor(BigIP_Object):
-    def __init__(self, name, partition, monitor, send=None, interval=None):
+    def __init__(self, name, partition, monitor, send_string=None, interval=None):
+        self.name = name
+        self.partition = partition
         self.monitor = monitor
-        self.send = send
+        self.send_string = send_string
         self.interval = interval
 
 
 class Pool(BigIP_Object):
-    def __init__(
-        self,
-        name,
-        partition,
-        monitor,
-        members,
-        member_name,
-    ):
+    def __init__(self, name, partition, monitor, members, member_name):
+        self.name = name
+        self.partition = partition
+        self.monitor = monitor
         self.members = members
-        self.member_name
+        self.member_name = member_name
 
 
-class Virtual_server(BigIP_Object):
+class Virtual_Server(BigIP_Object):
     def __init__(self, name, partition, protocol, pool, profiles, addr_translate):
+        self.name = name
+        self.partition = partition
         self.protocol = protocol
         self.pool = pool
         self.profiles = profiles
